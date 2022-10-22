3a1654b2390f2831a7da66a07ee9d5f5d277664c
Jason Costello <jason.costello@synapt.ec>
2022-10-22 10:16:21 +0100

    Adding MVP config loading from YAML

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.9-slim-buster
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY cronsql.py .
+COPY config.yaml .
 CMD ["python", "./cronsql.py"]
--- cronsql.py
+++ cronsql.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import sqlalchemy.exc
 from sqlalchemy import create_engine
+import yaml
 
 engine = create_engine("postgresql://postgres:postgres@database:5432/postgres")
 
 
+def load_config():
+    config = None
+    with open("./config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            print(e)
+    return config
+
+
 def ping():
+    config = load_config()
+
+    if not config:
+        return "error"
+
     try:
-        result = pd.read_sql(f"""
-            SELECT * 
-            FROM example 
-            WHERE timestamp > NOW() - INTERVAL '10 seconds';
-        """, engine)
+        result = pd.read_sql(config["query"], engine)
         return result
     except sqlalchemy.exc.OperationalError:
         return None
