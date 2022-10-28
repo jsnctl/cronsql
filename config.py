f75de48267599c24d44ca2efbc40be8259725b55
Jason Costello <jason.costello@synapt.ec>
2022-10-28 15:06:08 +0100

    [WIP] Adding error.py, ConfigNotFoundException

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import yaml
 
+from error import ConfigNotFoundException
+
 
 def load_config():
     with open("config.yaml", "r") as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as e:
-            print(e)
+            raise ConfigNotFoundException
     return config
